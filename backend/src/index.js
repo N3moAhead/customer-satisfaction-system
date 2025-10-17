c8e0e9414e76e85b0427e62cad120134c1eba3e4
Lukas Heberling <62028575+N3moAhead@users.noreply.github.com>
2025-10-17 17:25:35 +0200

    feat: Some Changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,13 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 const port = 2509;
+
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to the Customer-Satisfaction-Backend</h1>");
